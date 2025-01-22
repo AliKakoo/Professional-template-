97aed145b93000aaa3ce1f7f5f1c23e150ffc12a
AliKakoo <alikakuo@yahoo.com>
2025-01-22 19:13:10 +0330

    create files

---
+++ scripts/scripts.js
@@ -0,0 +1,19 @@
+// for open or close hamberger buttons
+
+const navBtn = document.querySelector(".nav__btn");
+const navMenu = document.querySelector(".nav-menu");
+
+let navOpen = false;
+
+navBtn.addEventListener("click", () => {
+  if (!navOpen) {
+    navBtn.classList.add("nav__btn--open");
+    navMenu.classList.add("nav-menu--open");
+    
+    navOpen = true;
+} else {
+    navBtn.classList.remove("nav__btn--open");
+    navMenu.classList.remove("nav-menu--open");
+    navOpen = false;
+  }
+});
